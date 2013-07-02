21040f27fee6cad15844d0935c987aed8b642ffd
Denis Dydychkin <nyrl@mail.ru>
2013-07-02 19:38:33 +0400

    Added dependencies for demos

--- demos.mk
+++ demos.mk
@@ -2,6 +2,7 @@
 HEADERS=$(shell find include -name *.h -o -name *.hpp)
 DEMOS_SRC=$(shell find demos -name demo_*.cpp)
 DEMOS=$(subst .cpp,,$(basename $(notdir $(DEMOS_SRC))))
+DEMOS_HEADERS=$(shell find demos -name *.h -o -name *.hpp)
 
 LIBV4L2?=
 LIBV4L2_INCDIR?=$(LIBV4L2)/include
@@ -17,10 +18,10 @@ clean: $(addprefix clean-,$(DEMOS))
 
 
 
-demo_resample_bicubic_v4l2_to_file: demos/demo_resample_bicubic_v4l2_to_file.cpp $(HEADERS) $(LIBV4L2_INCDIR)/libv4l2.h
+demo_resample_bicubic_v4l2_to_file: demos/demo_resample_bicubic_v4l2_to_file.cpp $(HEADERS) $(DEMOS_HEADERS)
 	$(CXX) -I . -I include -I demos -I $(LIBV4L2_INCDIR) -L $(LIBV4L2_LIBDIR) -std=c++0x -g -o $@ $< -lv4l2
 
-demo_%: demos/demo_%.cpp $(HEADERS)
+demo_%: demos/demo_%.cpp $(HEADERS) $(DEMOS_HEADERS)
 	$(CXX) -I . -I include -g -o $@ $<
 
 clean-demo_%:
